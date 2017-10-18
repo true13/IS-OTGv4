611c9bf378c1a903c3f1198a7a1f6923f4a0706b
true13 <true1982@naver.com>
2017-10-18 21:01:06 +0900

    IG update
    IG Framework

--- InformationGathering/InformationGatheringedit.py
+++ InformationGathering/InformationGatheringedit.py
@@ -1,11 +1,14 @@
 import requests
 from bs4 import BeautifulSoup
-
+from bs4 import Comment
+import re
 
 class InformationGathering:
     def __init__(self, target_url):
         self.url = target_url
-        self.search_engine()
+     #   self.search_engine()
+     #   self.web_sever()
+        self.framework()
 
     def search_engine(self):
         search_engine = ['http://www.google.com/search', 'http://www.bing.com/search']
@@ -34,52 +37,60 @@ class InformationGathering:
 
         print("Google" + str(title_google))
         print("Bing" + str(title_bing))
-		
-		
-	def web_sever (self):
-		req = requests.get(self.url)
-		status = req.status_code
-		
-		if  200 != status:
-			print("Connect Error: ", end=' ')
-			print(status)
-		else:
-			print(req.headers['Server'])
-			
-		
-			
-	#framework check	
-	def framework (self):		
-		req = requests.get(self.url)
-		
-		#HTTP headers
-		if  req.headers['Accept-Encoding'] != ''
-			print(req.headers['Accept-Encoding'])
-		
-		#cookies
-		cooki = req.cookies
-		framework_type_cooki = ['zope3' , 'cakephp', 'accuracy', 'kohanasession', 'laravel_session']
-		framework_type_cooki_result = []
-		
-		for x in range(1,4)
-			if cooki == framework_type_cooki[x]
-				framework_type_result[x] =1
-			else
-				framework_type_result[x] =0
-				
-		#HTML Source
-		soup = BeautifulSoup(url, 'html.parser')	
-		framework_type_html = ['<!- START header.Tags.cfm' ,'_ _VIEWSTATE' ,'<!-ZK' ,'<!- BC_OBNW ->' ,'ndxz-studio' ]
-		framework_type_html_result = [] 
-		for x in range(0,4)
-			if 	'' !=soup.find_all(framework_type_html[x])
-				framework_type_html_result[x]= 1
-			else
-				framework_type_html_result[x]= 0
-		
+
+    def web_sever(self):
+        req = requests.get(self.url)
+        status = req.status_code
+
+        if 200 != status:
+            print("Connect Error: ", end=' ')
+            print(status)
+        else:
+            print(req.headers['Server'])
+
+            # framework check
+
+    def framework(self):
+        req = requests.get(self.url)
+
+        # HTTP headers
+
+        if 'Accept-Encoding' in req.headers:
+            print(req.headers['Accept-Encoding'])
+
+        # cookies
+
+        if 'X-Powered-By' in req.cookies:
+            print(req.headers['X-Powered-By'])
 
 
+        """
+        framework_type_cooki = ['zope3', 'cakephp', 'accuracy', 'kohanasession', 'laravel_session']
+        framework_type_cooki_result = []
 
+        for x in range(0, 4):
+            if cooki == framework_type_cooki[x]:
+                framework_type_cooki_result[x] = 1
+            else:
+                framework_type_cooki_result[x] = 0
+        """
+
+
+        # HTML Source
+        html = requests.get(self.url)
+        soup = BeautifulSoup(html.content, 'html.parser')
+
+        framework_type_html = ['<!- START header.Tags.cfm', '_ _VIEWSTATE', '<!-ZK', '<!- BC_OBNW ->', 'ndxz-studio']
+        framework_type_html_result = []
+        print(soup.find_all(string=lambda text:isinstance(text,Comment)))
+
+    """
+        for x in range(0, 4):
+            if soup.find_all(framework_type_html[x]):
+                framework_type_html_result[x] = 1
+            else:
+                framework_type_html_result[x] = 0
+    """
 
 if __name__ == "__main__":
-    IG = InformationGathering("jbnu.ac.kr")
+    IG = InformationGathering("http://iscert.org")
